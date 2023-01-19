9174b7c423c53079ccf762473604dd3378e05db2
codigohache01@gmail.com <codigohache@MacBook-Air-de-Humberto.local>
2023-01-19 17:14:41 -0300

    Mi porfolio en la red

---
+++ java/questions.js
@@ -0,0 +1,24 @@
+(function(){
+    const titleQuestion = [...document.querySelectorAll('.questions_title')];
+console.log(titleQuestion)
+
+titleQuestion.forEach(question =>{
+    question.addEventListener('click', ()=>{
+        let height = 0;
+        let answer = question.nextElementSibling;
+        let addPadding = question.parentElement.parentElement
+
+        addPadding.classList.toggle('questions_padding--add')
+
+        question.children[0].classList.toggle('questions_arrow--rotate')
+
+        if(answer.clientHeight === 0){
+            height = answer.scrollHeight;
+
+        }
+
+        answer.style.height =`${height}px`
+    })
+});
+   
+})();
